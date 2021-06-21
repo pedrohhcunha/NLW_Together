ca151f3630a28890d5a71f6b90b2c1350a88c3ce
pedrohhcunha <phhcpedrohhcunha@gmail.com>
2021-06-21 20:53:07 -0300

    use props

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import { Button } from './components/button'
+
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <div>
+      <h1>Hello World!</h1>
+      <Button text="Button 1" />
+      <Button text="Button 2" />
+      <Button>String</Button>
+    </div>
   );
 }
 
---
+++ src/components/button.tsx
@@ -0,0 +1,10 @@
+type ButtonProps = {
+    text?: string;
+    children?: string;
+}
+
+export function Button(props: ButtonProps) {
+    return (
+        <button>{props.text || props.children}</button>
+    )
+}
